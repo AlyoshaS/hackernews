344384558505b24da7a5945970fb38f99b0d81e1
alyoshas <evelynschmitz1@gmail.com>
2018-08-21 11:22:04 -0300

    Updated code.

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,9 @@ const APP_SECRET = 'GraphQL-is-aw3some'
 function getUserId(context) {
     const Authorization = context.request.get('Authorization')
     if (Authorization) {
-        const token = Authorization.replace('Bearer ', '')
-        const {
-            userId
-        } = jwt.verify(token, APP_SECRET)
-        return userId
+      const token = Authorization.replace('Bearer ', '')
+      const { userId } = jwt.verify(token, APP_SECRET)
+      return userId
     }
 
     throw new Error('Not authenticated')
