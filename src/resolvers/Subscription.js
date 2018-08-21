bdd4c6ee27bf5cc860db1ebfc6a0190bce8aec4a
alyoshas <evelynschmitz1@gmail.com>
2018-08-21 12:04:35 -0300

    Added newVote subscription.

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -5,10 +5,22 @@ function newLinkSubscribe(parent, args, context, info) {
     )
 }
 
+function newVoteSubscribe (parent, args, context, info) {
+    return context.db.subscrition.vote(
+        { where: { mutation_in: ['CREATED'] } },
+        info,
+    )
+}
+
 const newLink = {
     subscribe: newLinkSubscribe
 }
 
+const newVote = {
+    subscribe: newVoteSubscribe
+}
+
 module.exports = {
     newLink,
+    newVote,
 }
